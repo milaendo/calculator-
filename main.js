5dfde1aa208da52e774e4bf953336560eb45ac84
Mila Endo <milal.alim@gmail.com>
2017-07-14 10:49:02 -0700

    finished html and css

---
+++ main.js
@@ -0,0 +1,22 @@
+document.querySelector (button)
+element.addEventListener("click", displayFunction)
+	function displayFunction = 
+
+
+function displayFunction(element) {
+
+}
+
+let displayFunction = function(element) {
+
+}
+
+
+element.addEventListener("click", function(element) {
+	//define logic in here
+});
+//i know i want to display the value of the number in the text field
+//value of button add to the the inner html of display 
+//if C reset inerhtml in display the rest inner html add to previous in display
+// if equal do the math and show answer in display 
+//
