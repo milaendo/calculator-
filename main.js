d158b1237eb21b3628fd50bec1b5b0c900a5e00e
Mila Endo <milal.alim@gmail.com>
2017-07-14 14:04:50 -0700

    buttons arent working

--- main.js
+++ main.js
@@ -1,22 +1,33 @@
-document.querySelector (button)
-element.addEventListener("click", displayFunction)
-	function displayFunction = 
+let buttons = document.querySelectorAll('button')
+	console.log (buttons)
+
+buttons.forEach(function(action){
+	//i know i want to display the value of the number in the text field
+	action.addEventListener("click", function(action) {
+
+			let screen = document.getElementById ('display')
+			let btnVal = this.innerHTML
+			// if equal do the math and show answer in display 
+			let equation = screen.innerHTML
+			//value of button add to the the inner html of display 
+			//if C reset inerhtml in display the rest inner html add to previous in display
+		if (btnVal === 'C') {
+			screen.innerHTML = ''
+		}
+		else if (btnVal === '=') {
+			let input = equation
+			
+		}
+		else {
+			screen.innerHTML = btnVal
+		}
+		console.log (action) 
+		console.log (action.target.innerHTML)
+	});
+})
+
+//display the value of inner html onto the display
+//define operators and their functions
+//be able to solve the string in the display
 
 
-function displayFunction(element) {
-
-}
-
-let displayFunction = function(element) {
-
-}
-
-
-element.addEventListener("click", function(element) {
-	//define logic in here
-});
-//i know i want to display the value of the number in the text field
-//value of button add to the the inner html of display 
-//if C reset inerhtml in display the rest inner html add to previous in display
-// if equal do the math and show answer in display 
-//
