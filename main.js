a43de1a597c4df02f9c43312f1f5fadeaf6eedcc
Mila Endo <milal.alim@gmail.com>
2017-07-14 17:59:06 -0700

    fixed clear button

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 let buttons = document.querySelectorAll('button')
-	console.log (buttons)
-
+	
 buttons.forEach(function(action){
 	//i know i want to display the value of the number in the text field
 	action.addEventListener("click", function(action) {
@@ -11,23 +10,23 @@ buttons.forEach(function(action){
 			let equation = screen.innerHTML
 			//value of button add to the the inner html of display 
 			//if C reset inerhtml in display the rest inner html add to previous in display
+			
 		if (btnVal === 'C') {
 			screen.innerHTML = ''
 		}
 		else if (btnVal === '=') {
 			let input = equation
-			
 		}
+		//display the value of inner html onto the display
 		else {
-			screen.innerHTML = btnVal
+			screen.innerHTML += btnVal
 		}
-		console.log (action) 
-		console.log (action.target.innerHTML)
+		
 	});
 })
 
-//display the value of inner html onto the display
 //define operators and their functions
+//have operators hold previously typed numbers
 //be able to solve the string in the display
 
 
