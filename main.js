04b35b9031d4129afbe4e27024a039141bae86fd
Mila Endo <milal.alim@gmail.com>
2017-07-17 20:43:37 -0700

    took out loop

--- main.js
+++ main.js
@@ -1,49 +1,57 @@
-let buttons = document.querySelectorAll('button')
-	
-buttons.forEach(function(action){
-	//i know i want to display the value of the number in the text field
-	action.addEventListener("click", function(action) {
+let form = document.getElementById('calc')
+let display = document.getElementById('display')
+let num1 = num2 = operator = ''
 
-			let screen = document.getElementById ('display')
-			let btnVal = this.innerHTML
-			// if equal do the math and show answer in display 
-			let equation = screen.innerHTML
-			//value of button add to the the inner html of display 
-			//if C reset inerhtml in display the rest inner html add to previous in display
+form.addEventListener('click', function(e){
+	if (e.target.id !== 'display') {
+		let btnVal = e.target.innerHTML
+		//i know i want to display the value of the number in the text field
+		display.innerHTML += btnVal
 
 		if (btnVal === 'C') {
-			screen.innerHTML = ''
-		}
-		else if (btnVal === '='){
-			return domath
-		}
-		//display the value of inner html onto the display
-		else {
-			screen.innerHTML += btnVal
-		}
-
-		function domath () {
-			let num1 = Number()
-			let num2 = Number()
-			if (btnVal === '/') {
-			return num1 / num2
+			display.innerHTML = ''
+			num1 = ''
+			num2 = ''
+			operator = ''
+			//have operators hold previously typed numbers
+		} else if (btnVal === '=') {
+			let val = doMath() + ''
+			display.innerHTML = val
+			num1 = val
+			opeartor = ''
+			num2 = ''
+			//define operators and their functions
+		} else if (btnVal === '+') {
+			operator = '+'
+		} else if (btnVal === '-') {
+			operator = '-'
+		} else if (btnVal === '*') {
+			operator = '*'
+		} else if (btnVal === '/') {
+			operator = '/'
+		} else {
+			// button is a number or a decimal
+			if (operator === '') {
+				num1 += btnVal
+			} else {
+				num2 += btnVal
 			}
-			if (btnVal === '*') {
-				return num1 * num2
-			}
-			if (btnVal === '+') {
-				return num1 + num2
-			}
-			if (btnVal === '-') {
-				return num1 - num2
-			}
 		}
-
-	});
+	}
 })
-
-//define operators and their functions
-//have operators hold previously typed numbers
+	
 //be able to solve the string in the display
+function doMath() {
+	if (operator === '+') {
+		return Number(num1) + Number(num2)
+	} else if (operator === '-') {
+		return Number(num1) - Number(num2)
+	} else if (operator === '*') {
+		return Number(num1) * Number(num2)
+	} else if (operator === '/') {
+		return Number(num1) / Number(num2)
+	}
+}
+
 
 
